b967de193e7bbacfa50a98fca5a0102e42ff9756
Topper Bowers <topper@toppingdesign.com>
2024-03-25 12:04:09 +0100

    no blanks

--- souls/samantha-learns/soul/initialProcess.ts
+++ souls/samantha-learns/soul/initialProcess.ts
@@ -6,8 +6,6 @@ import { externalDialog } from "./lib/cognitiveSteps";
 const provokesSpeaker: MentalProcess = async ({ workingMemory }) => {
   const { speak  } = useActions()
 
-
-
   const [withDialog, stream] = await externalDialog(workingMemory, "Talk to the user trying to gain trust and learn about their inner world.", { stream: true, model: "quality" });
   speak(stream);
 
